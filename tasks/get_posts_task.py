67bf2fb4e009f79133521e1f4b672574286a6546
Luis <luchitofmos@gmail.com>
2025-06-08 15:26:01 -0500

    Fix performance test, update get_posts_task requirementes gitignore files

--- tasks/get_posts_task.py
+++ tasks/get_posts_task.py
@@ -1,6 +1,7 @@
 import logging
 from locust import HttpUser, task, between
 from config.settings import HOST
+import json
 
 # Logs configuration
 logging.basicConfig(
@@ -15,13 +16,40 @@ class GetPostsUser(HttpUser):
 
     @task
     def get_posts(self):
-        with self.client.get("/posts", catch_response=True) as response:
+        endpoint = "/posts"
+        logger.info("Executing")
+        with self.client.get(endpoint, catch_response=True) as response:
             if response.status_code == 200:
-                logger.info(f"Succesful - GET {response.url} - Status Code {response.status_code}")
-                response.success()
+                try:
+                    data = response.json()
+                    if isinstance(data, list) and len(data) == 100:
+                        logger.info(
+                            f"Successful: GET {response.url} - Status Code: {response.status_code} - "
+                            f"Posts received: {len(data)}"
+                        )
+                        logger.info("Response Json:\n" + json.dumps(data[3], indent=2))
+                        response.success()
+                    elif isinstance(data, list):
+                        logger.warning(
+                            f"Incorrect Quantity: GET {response.url} - Status Code: {response.status_code}"
+                        )
+                        response.failure(f"100 posts were expected. They just arrived: {len(data)}")
+                    else:
+                        logger.error(
+                            f"Format Error: GET {response.url} - Status Code: {response.status_code}"
+                            f"Content (200 chars): {response.text[:200]}"
+                        )
+                        response.failure(f"Answer is not a list. The type is: {type(data)}")
+                except ValueError:
+                    logger.error(
+                        f"Json Error: GET {response.url} - Status Code: {response.status_code}"
+                        f"Failed to decode response as JSON"
+                        f"Content (200 chars): {response.text[:200]}"
+                    )
+                    response.failure("Response is not valid JSON")
             else:
                 logger.error(
-                    f"Error - GET {response.url} - Status Code {response.status_code} - "
-                    f"Response: {response.text}"
+                    f"Http Error: GET {response.url} - Status Code: {response.status_code}"
+                    f"Content (200 chars): {response.text[:200]}"
                 )
-                response.failure(f"Unexpected Code: {response.status_code}")
+                response.failure(f"Unexpected HTTP code: {response.status_code}")
